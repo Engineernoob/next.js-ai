8328bc36702ad73d68c5de1039d017d74645e4f7
Taahirah Denmark <t.denmark@outlook.com>
2024-10-24 17:14:40 -0400

    change the api endpoint

--- ai-web/frontend/my-ai/pages/api/assistant.js
+++ ai-web/frontend/my-ai/pages/api/assistant.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const response = await fetch('http://localhost:5000/api/assistant', {
+      const response = await fetch(' http://127.0.0.1:5000/api/assistant', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
